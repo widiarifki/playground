07b83c6efe378012b0b10daff4b21ead4de0e67b
widiarifki <widiarifki@gmail.com>
2021-04-30 01:37:08 +0700

    set immediate upload &  notification

--- app/src/main/java/com/widiarifki/playground/MainActivity.kt
+++ app/src/main/java/com/widiarifki/playground/MainActivity.kt
@@ -2,28 +2,26 @@ package com.widiarifki.playground
 
 import android.Manifest
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import androidx.work.*
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.ByteArrayOutputStream
 
 class MainActivity : Activity() {
-
-    lateinit var storage: FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        storage = Firebase.storage
         reqPermission()
         setEvent()
     }
@@ -41,21 +39,19 @@ class MainActivity : Activity() {
 
     private fun setEvent() {
         btnUpload.setOnClickListener {
-            setupWorker()
+            if (isConnected() == true) {
+                immediateUpload()
+            } else {
+                setupUploadWorker()
+            }
         }
     }
 
-    private fun saveFile(bitmap: Bitmap, fileName: String) {
-        openFileOutput(fileName, Context.MODE_PRIVATE).use { fos ->
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-        }
-    }
-
-    private fun setupWorker() {
+    private fun setupUploadWorker() {
         // Local work
-        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
-        val filename = System.currentTimeMillis().toString()
-        saveFile(bitmap, filename)
+        val filename = getBitmapFilename()
+
+        createNotification("Upload pending untuk file ${filename}")
 
         // workmanager - 0. (optional) assign param data
         val inputData = Data.Builder().apply {
@@ -76,5 +72,69 @@ class MainActivity : Activity() {
 
         // workmanager - 2. enqueue the workrequest
         WorkManager.getInstance(applicationContext).enqueue(uploadWorkRequest)
+    }
+
+    private fun immediateUpload() {
+        val filename = getBitmapFilename()
+
+        val firebaseStorage = Firebase.storage
+        val storageRef = firebaseStorage.reference
+        val fileRef = storageRef.child("$filename.jpg") // image type masih hardcode
+        val fileData = applicationContext.openFileInput(filename).readBytes()
+
+        fileRef.putBytes(fileData)
+            .addOnFailureListener {
+                setupUploadWorker()
+            }
+            .addOnSuccessListener { taskSnapshot ->
+                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                createNotification("File ${taskSnapshot.metadata?.name} berhasil diunggah")
+            }
+    }
+
+    private fun getBitmapFilename(): String? {
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val filename = System.currentTimeMillis().toString()
+        saveFile(bitmap, filename)
+        return filename
+    }
+
+    private fun saveFile(bitmap: Bitmap, fileName: String) {
+        openFileOutput(fileName, Context.MODE_PRIVATE).use { fos ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        }
+    }
+
+    private fun createNotification(message: String) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+        val builder = NotificationCompat.Builder(this, BuildConfig.APPLICATION_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Kabar upload")
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        createNotificationChannel()
+        notificationManager?.notify(0, builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = BuildConfig.APPLICATION_ID
+            val descriptionText = BuildConfig.APPLICATION_ID
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(BuildConfig.APPLICATION_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun isConnected() : Boolean? {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+        return connectivityManager?.activeNetworkInfo?.isConnected
     }
 }
--- app/src/main/java/com/widiarifki/playground/UploadWorker.kt
+++ app/src/main/java/com/widiarifki/playground/UploadWorker.kt
@@ -1,14 +1,17 @@
 package com.widiarifki.playground
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
-import android.util.Log
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.runBlocking
 
-class UploadWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+class UploadWorker(val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
 
     override fun doWork(): Result {
         val filename = inputData.getString(PARAM_FILENAME)
@@ -22,20 +25,45 @@ class UploadWorker(context: Context, workerParams: WorkerParameters) : Worker(co
             runBlocking {
                 fileRef.putBytes(fileData)
                     .addOnFailureListener {
-                        // Handle unsuccessful uploads
-                        Log.v("uploadTask", it.message.toString())
+                        createNotification("File gagal diunggah: ${it.message}")
                     }
                     .addOnSuccessListener { taskSnapshot ->
                         // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
-                        // ...
-                        Log.v("uploadTask", "success ${taskSnapshot.metadata?.name} " +
-                                "${taskSnapshot.metadata?.path}")
+                        createNotification("File ${taskSnapshot.metadata?.name} berhasil diunggah")
                     }
             }
 
             return Result.success()
         } ?: run {
             return Result.failure()
+        }
+    }
+
+    private fun createNotification(message: String) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+        val builder = NotificationCompat.Builder(applicationContext, BuildConfig.APPLICATION_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Kabar upload")
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        createNotificationChannel()
+        notificationManager?.notify(0, builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = BuildConfig.APPLICATION_ID
+            val descriptionText = BuildConfig.APPLICATION_ID
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(BuildConfig.APPLICATION_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
